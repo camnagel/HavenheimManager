1d397651810931260a11705560aa62f9d186849f
evevee <camwnagel@gmail.com>
2025-01-11 12:45:42 -0700

    Initial crafting changes

---
+++ src/Enums/Workshop.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace AssetManager.Enums
+{
+    public enum Workshop
+    {
+        [Description("None")]
+        None,
+        [Description("Improvised")]
+        Improvised,
+        [Description("Basic")]
+        Basic,
+        [Description("Masterwork")]
+        Masterwork,
+        [Description("Guild")]
+        Guild
+    }
+}
---
+++ src/Handlers/CraftHandler.cs
@@ -0,0 +1,29 @@
+﻿using AssetManager.Enums;
+
+namespace AssetManager.Handlers;
+
+public class CraftHandler
+{
+    private readonly MainWindowViewModel _vm;
+
+    private string _itemName;
+
+    public CraftHandler(MainWindowViewModel vm)
+    {
+        _vm = vm;
+    }
+
+    internal void SetItemName(string itemName)
+    {
+        _itemName = itemName;
+    }
+
+    internal void CraftToolAction(object arg)
+    {
+        if (arg is string filter)
+        {
+            Core toggleCore = filter.StringToCore();
+
+        }
+    }
+}
